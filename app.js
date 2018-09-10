bb63fdaa018227eb9d29f58d330f38623f032703
frank.park <frank.park@kakaocorp.com>
2018-09-11 02:25:46 +0900

    쿼리타입 & 리졸브 추가

--- app.js
+++ app.js
@@ -13,8 +13,23 @@ const userType = new Graphql.GraphQLObjectType({
         isVacation: {type: Graphql.GraphQLBoolean}
     }
 });
+
+const queryType = new Graphql.GraphQLObjectType({
+    name: "Query",
+    fields: {
+        allUser: {
+            type: new Graphql.GraphQLList(userType),
+            resolve: () => user
+        }
+    }
+});
+
 app.use(express.static('public'));
 app.use('/graphql', graphqlHTTP({
+    schema: new Graphql.GraphQLSchema({
+        query: queryType
+    })
 }));
+
 app.listen(4000);
 console.log('Running a GraphQL server');
