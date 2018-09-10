9ea57bd7b6158513ea0fea76c87352c70037e8ba
frank.park <frank.park@kakaocorp.com>
2018-09-11 02:24:52 +0900

    user 데이터 추가

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 import express from 'express'
 import graphqlHTTP from 'express-graphql'
-import 'graphql'
+import * as Graphql from 'graphql'
+import {user} from './fakeDB'
 
 const app = express();
+
+const userType = new Graphql.GraphQLObjectType({
+    name: "User",
+    fields: {
+        name: {type: Graphql.GraphQLString},
+        username: {type: Graphql.GraphQLString},
+        isVacation: {type: Graphql.GraphQLBoolean}
+    }
+});
 app.use(express.static('public'));
 app.use('/graphql', graphqlHTTP({
 }));
---
+++ fakeDB.js
@@ -0,0 +1,68 @@
+const user = [
+    {
+        name: "한종진",
+        username: "brandon.han",
+        isVacation: true,
+    },
+    {
+        name: "김홍규",
+        username: "argus.q",
+        isVacation: false,
+    },
+    {
+        name: "장준석",
+        username: "august.chang",
+        isVacation: false,
+    },
+    {
+        name: "전호석",
+        username: "chris.jeun",
+        isVacation: false,
+    },
+    {
+        name: "이상헌",
+        username: "colin.sh",
+        isVacation: false,
+    },
+    {
+        name: "김창호",
+        username: "daniel.94",
+        isVacation: false,
+    },
+    {
+        name: "김덕균",
+        username: "derek.kim",
+        isVacation: false,
+    },
+    {
+        name: "오창연",
+        username: "ed.banger",
+        isVacation: false,
+    },
+    {
+        name: "박찬진",
+        username: "frank.park",
+        isVacation: false,
+    },
+    {
+        name: "이동준",
+        username: "genos.lee",
+        isVacation: false,
+    },
+    {
+        name: "이광연",
+        username: "kai.1001",
+        isVacation: false,
+    },
+    {
+        name: "정지선",
+        username: "keira.j",
+        isVacation: false,
+    },
+    {
+        name: "강경보",
+        username: "peter.kang",
+        isVacation: false,
+    }];
+
+export {user}
